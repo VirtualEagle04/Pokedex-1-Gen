a169d2770b25aa1f27c4c0cc926e5870e76b3f15
Javi1f <117606366+Javi1f@users.noreply.github.com>
2023-03-03 23:01:50 -0500

    funcionalidad de agregar de doble tipo o sencillo
    faltan las excepciones y la funcion mostrar para que evidentemente se muestre en el jlist

--- Pokedex/src/co/edu/unbosque/controller/Controller.java
+++ Pokedex/src/co/edu/unbosque/controller/Controller.java
@@ -3,40 +3,47 @@ package co.edu.unbosque.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import co.edu.unbosque.model.PokemonDAO;
 import co.edu.unbosque.view.VentanaPrincipal;
 
-public class Controller implements ActionListener{
-	
+public class Controller implements ActionListener {
+
 	private VentanaPrincipal vp;
-	
+	private PokemonDAO pdao;
+
 	public Controller() {
-		
+
 	}
+
 	public void run() {
 		vp = new VentanaPrincipal();
+		pdao = new PokemonDAO();
 		agregarLectores();
 	}
-	
+
 	public void agregarLectores() {
 		vp.getPanel_crud().getAgregar().addActionListener(this);
 		vp.getPanel_crud().getAgregar().setActionCommand("CrudAgregar");
-		
+
+		vp.getPanel_agregar().getConfirmar().addActionListener(this);
+		vp.getPanel_agregar().getConfirmar().setActionCommand("AgregConfirmar");
+
 		vp.getPanel_crud().getEliminar().addActionListener(this);
 		vp.getPanel_crud().getEliminar().setActionCommand("CrudEliminar");
-		
+
 		vp.getPanel_crud().getMostrar().addActionListener(this);
 		vp.getPanel_crud().getMostrar().setActionCommand("CrudMostrar");
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		switch(e.getActionCommand()) {
+		switch (e.getActionCommand()) {
 		case "CrudAgregar": {
 			System.out.println("Agregar");
 			vp.getPanel_agregar().setVisible(true);
 			vp.getPanel_mostrar().setVisible(false);
 			vp.getPanel_eliminar().setVisible(false);
-			
+
 			break;
 		}
 		case "CrudEliminar": {
@@ -44,7 +51,7 @@ public class Controller implements ActionListener{
 			vp.getPanel_agregar().setVisible(false);
 			vp.getPanel_mostrar().setVisible(false);
 			vp.getPanel_eliminar().setVisible(true);
-			
+
 			break;
 		}
 		case "CrudMostrar": {
@@ -54,9 +61,105 @@ public class Controller implements ActionListener{
 			vp.getPanel_eliminar().setVisible(false);
 			break;
 		}
-		
+		case "AgregConfirmar": {
+
+			System.out.println("Agregado correctamente");
+
+			System.out.println(vp.getPanel_agregar().getCampo_tipo_sec().getSelectedItem());
+
+			if (vp.getPanel_agregar().getCampo_tipo_sec().getSelectedItem().equals("Sin 2' Tipo")) {
+
+				String id_temp = vp.getPanel_agregar().getCampo_id().getText();
+				int id = Integer.parseInt(id_temp);
+
+				String nombre = vp.getPanel_agregar().getCampo_nombre().getText();
+
+				String lv_temp = vp.getPanel_agregar().getCampo_lv().getText();
+				int lv = Integer.parseInt(lv_temp);
+
+				String al_temp = vp.getPanel_agregar().getCampo_al().getText();
+				float al = Float.parseFloat(al_temp);
+
+				String pe_temp = vp.getPanel_agregar().getCampo_pe().getText();
+				float pe = Float.parseFloat(pe_temp);
+
+				String hp_temp = vp.getPanel_agregar().getCampo_hp().getText();
+				int hp = Integer.parseInt(hp_temp);
+
+				String atk_temp = vp.getPanel_agregar().getCampo_atk().getText();
+				int atk = Integer.parseInt(atk_temp);
+
+				String def_temp = vp.getPanel_agregar().getCampo_def().getText();
+				int def = Integer.parseInt(def_temp);
+
+				String satk_temp = vp.getPanel_agregar().getCampo_satk().getText();
+				int satk = Integer.parseInt(satk_temp);
+
+				String sdef_temp = vp.getPanel_agregar().getCampo_sdef().getText();
+				int sdef = Integer.parseInt(sdef_temp);
+
+				String vel_temp = vp.getPanel_agregar().getCampo_vel().getText();
+				int vel = Integer.parseInt(vel_temp);
+
+				String tipo1 = vp.getPanel_agregar().getCampo_tipo().getSelectedItem() + "";
+				
+				String desc = vp.getPanel_agregar().getCampo_descripcion().getText();
+
+				pdao.crearTipoIndividual(id + "", nombre, lv, al, pe, tipo1, hp, atk, def, satk, sdef, vel, desc);
+				System.out.println(pdao.getLista().get(0));
+
+				break;
+
+			} else {
+
+				String id_temp = vp.getPanel_agregar().getCampo_id().getText();
+				int id = Integer.parseInt(id_temp);
+
+				String nombre = vp.getPanel_agregar().getCampo_nombre().getText();
+
+				String lv_temp = vp.getPanel_agregar().getCampo_lv().getText();
+				int lv = Integer.parseInt(lv_temp);
+
+				String al_temp = vp.getPanel_agregar().getCampo_al().getText();
+				float al = Float.parseFloat(al_temp);
+
+				String pe_temp = vp.getPanel_agregar().getCampo_pe().getText();
+				float pe = Float.parseFloat(pe_temp);
+
+				String hp_temp = vp.getPanel_agregar().getCampo_hp().getText();
+				int hp = Integer.parseInt(hp_temp);
+
+				String atk_temp = vp.getPanel_agregar().getCampo_atk().getText();
+				int atk = Integer.parseInt(atk_temp);
+
+				String def_temp = vp.getPanel_agregar().getCampo_def().getText();
+				int def = Integer.parseInt(def_temp);
+
+				String satk_temp = vp.getPanel_agregar().getCampo_satk().getText();
+				int satk = Integer.parseInt(satk_temp);
+
+				String sdef_temp = vp.getPanel_agregar().getCampo_sdef().getText();
+				int sdef = Integer.parseInt(sdef_temp);
+
+				String vel_temp = vp.getPanel_agregar().getCampo_vel().getText();
+				int vel = Integer.parseInt(vel_temp);
+
+				String tipo1 = vp.getPanel_agregar().getCampo_tipo().getSelectedItem() + "";
+
+				String tipo2 = vp.getPanel_agregar().getCampo_tipo_sec().getSelectedItem() + "";
+
+				String desc = vp.getPanel_agregar().getCampo_descripcion().getText();
+
+				pdao.crearTipoSecundario(id + "", nombre, lv, al, pe, tipo1, tipo2, hp, atk, def, satk, sdef, vel,
+						desc);
+
+			}
+
+			break;
+		}
+
 		default:
 		}
-		
+
 	}
 }
--- Pokedex/src/co/edu/unbosque/view/PanelAgregar.java
+++ Pokedex/src/co/edu/unbosque/view/PanelAgregar.java
@@ -13,180 +13,203 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class PanelAgregar extends JPanel{
-	
+public class PanelAgregar extends JPanel {
+
 	private JPanel panel_agregar, panel_creacion;
-	private JLabel ind_id, ind_nombre, ind_lv, ind_hp, ind_atk, ind_def, ind_satk, ind_sdef, ind_vel, ind_tipo, ind_tipo_sec, ind_descripcion; //Indicadores
-	private JTextField campo_id, campo_nombre, campo_lv, campo_hp, campo_atk, campo_def, campo_satk, campo_sdef, campo_vel; //Campos
+	private JLabel ind_id, ind_nombre, ind_lv, ind_al, ind_pe, ind_hp, ind_atk, ind_def, ind_satk, ind_sdef, ind_vel,
+			ind_tipo, ind_tipo_sec, ind_descripcion; // Indicadores
+	private JTextField campo_id, campo_nombre, campo_lv, campo_al, campo_pe, campo_hp, campo_atk, campo_def, campo_satk,
+			campo_sdef, campo_vel; // Campos
 	private JTextArea campo_descripcion;
-	private JComboBox<String> campo_tipo, campo_tipo_sec; 
+	private JComboBox<String> campo_tipo, campo_tipo_sec;
 	private JButton confirmar;
 	private Font fuente;
-	
+
 	public PanelAgregar() {
-		
+
 		try {
 			fuente = Font.createFont(Font.TRUETYPE_FONT, new File("src/Assets/Font/Pokemon Classic.ttf"));
-		}catch (FontFormatException e) {
+		} catch (FontFormatException e) {
 			e.printStackTrace();
-		}catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		setBounds(0, 50, 720, 670);
 		setBackground(Color.DARK_GRAY);
 		setLayout(null);
-		
+
 		ind_id = new JLabel("ID");
 		ind_id.setBounds(30, 10, 100, 15);
 		ind_id.setFont(fuente);
 		ind_id.setFont(ind_id.getFont().deriveFont(Font.BOLD, 15));
-		
+
 		campo_id = new JTextField();
 		campo_id.setEditable(true);
 		campo_id.setBounds(135, 10, 60, 20);
 		campo_id.setFont(fuente);
 		campo_id.setFont(campo_id.getFont().deriveFont(Font.BOLD, 15));
-		
+
 		ind_nombre = new JLabel("NOMBRE");
 		ind_nombre.setBounds(30, 30, 105, 15);
 		ind_nombre.setFont(fuente);
 		ind_nombre.setFont(ind_nombre.getFont().deriveFont(Font.BOLD, 15));
-		
+
 		campo_nombre = new JTextField();
 		campo_nombre.setEditable(true);
 		campo_nombre.setBounds(135, 30, 150, 20);
 		campo_nombre.setFont(fuente);
 		campo_nombre.setFont(campo_nombre.getFont().deriveFont(Font.BOLD, 12));
-		
+
 		ind_lv = new JLabel("LV");
 		ind_lv.setBounds(30, 50, 100, 15);
 		ind_lv.setFont(fuente);
 		ind_lv.setFont(ind_lv.getFont().deriveFont(Font.BOLD, 15));
-		
+
 		campo_lv = new JTextField();
 		campo_lv.setEditable(true);
 		campo_lv.setBounds(135, 50, 60, 20);
 		campo_lv.setFont(fuente);
 		campo_lv.setFont(campo_lv.getFont().deriveFont(Font.BOLD, 15));
-		
+
+		ind_al = new JLabel("Alt");
+		ind_al.setBounds(30, 70, 60, 20);
+		ind_al.setFont(fuente);
+		ind_al.setFont(ind_al.getFont().deriveFont(Font.BOLD, 15));
+
+		campo_al = new JTextField();
+		campo_al.setEditable(true);
+		campo_al.setBounds(135, 70, 60, 20);
+		campo_al.setFont(fuente);
+		campo_al.setFont(ind_al.getFont().deriveFont(Font.BOLD, 15));
+
+		ind_pe = new JLabel("KG");
+		ind_pe.setBounds(30, 90, 60, 20);
+		ind_pe.setFont(fuente);
+		ind_pe.setFont(ind_al.getFont().deriveFont(Font.BOLD, 15));
+
+		campo_pe = new JTextField();
+		campo_pe.setEditable(true);
+		campo_pe.setBounds(135, 90, 60, 20);
+		campo_pe.setFont(fuente);
+		campo_pe.setFont(ind_al.getFont().deriveFont(Font.BOLD, 15));
+
 		ind_hp = new JLabel("HP");
-		ind_hp.setBounds(30, 70, 100, 15);
+		ind_hp.setBounds(30, 110, 100, 15);// 70
 		ind_hp.setFont(fuente);
 		ind_hp.setFont(ind_hp.getFont().deriveFont(Font.BOLD, 15));
-		
+
 		campo_hp = new JTextField();
 		campo_hp.setEditable(true);
-		campo_hp.setBounds(135, 70, 60, 20);
+		campo_hp.setBounds(135, 110, 60, 20);
 		campo_hp.setFont(fuente);
 		campo_hp.setFont(campo_hp.getFont().deriveFont(Font.BOLD, 15));
-		
+
 		ind_atk = new JLabel("ATK");
-		ind_atk.setBounds(30, 90, 100, 15);
+		ind_atk.setBounds(30, 130, 100, 15);
 		ind_atk.setFont(fuente);
 		ind_atk.setFont(ind_atk.getFont().deriveFont(Font.BOLD, 15));
-		
+
 		campo_atk = new JTextField();
 		campo_atk.setEditable(true);
-		campo_atk.setBounds(135, 90, 60, 20);
+		campo_atk.setBounds(135, 130, 60, 20);
 		campo_atk.setFont(fuente);
 		campo_atk.setFont(campo_atk.getFont().deriveFont(Font.BOLD, 15));
-		
+
 		ind_def = new JLabel("DEF");
-		ind_def.setBounds(30, 110, 100, 15);
+		ind_def.setBounds(30, 150, 100, 15);
 		ind_def.setFont(fuente);
 		ind_def.setFont(ind_def.getFont().deriveFont(Font.BOLD, 15));
-		
+
 		campo_def = new JTextField();
 		campo_def.setEditable(true);
-		campo_def.setBounds(135, 110, 60, 20);
+		campo_def.setBounds(135, 150, 60, 20);
 		campo_def.setFont(fuente);
 		campo_def.setFont(campo_def.getFont().deriveFont(Font.BOLD, 15));
-		
+
 		ind_satk = new JLabel("SP.ATK");
-		ind_satk.setBounds(30, 130, 100, 15);
+		ind_satk.setBounds(30, 170, 100, 15);
 		ind_satk.setFont(fuente);
 		ind_satk.setFont(ind_satk.getFont().deriveFont(Font.BOLD, 15));
-		
+
 		campo_satk = new JTextField();
 		campo_satk.setEditable(true);
-		campo_satk.setBounds(135, 130, 60, 20);
+		campo_satk.setBounds(135, 170, 60, 20);
 		campo_satk.setFont(fuente);
 		campo_satk.setFont(campo_satk.getFont().deriveFont(Font.BOLD, 15));
-		
+
 		ind_sdef = new JLabel("SP.DEF");
-		ind_sdef.setBounds(30, 150, 100, 15);
+		ind_sdef.setBounds(30, 190, 100, 15);
 		ind_sdef.setFont(fuente);
 		ind_sdef.setFont(ind_sdef.getFont().deriveFont(Font.BOLD, 15));
-		
+
 		campo_sdef = new JTextField();
 		campo_sdef.setEditable(true);
-		campo_sdef.setBounds(135, 150, 60, 20);
+		campo_sdef.setBounds(135, 190, 60, 20);
 		campo_sdef.setFont(fuente);
 		campo_sdef.setFont(campo_sdef.getFont().deriveFont(Font.BOLD, 15));
-		
+
 		ind_vel = new JLabel("VEL");
-		ind_vel.setBounds(30, 170, 60, 15);
+		ind_vel.setBounds(30, 210, 60, 15);
 		ind_vel.setFont(fuente);
 		ind_vel.setFont(ind_vel.getFont().deriveFont(Font.BOLD, 15));
-		
+
 		campo_vel = new JTextField();
 		campo_vel.setEditable(true);
-		campo_vel.setBounds(135, 170, 60, 20);
+		campo_vel.setBounds(135, 210, 60, 20);
 		campo_vel.setFont(fuente);
 		campo_vel.setFont(campo_vel.getFont().deriveFont(Font.BOLD, 15));
-		
+
 		ind_tipo = new JLabel("TIPO");
-		ind_tipo.setBounds(30, 192, 70, 15);
+		ind_tipo.setBounds(30, 230, 70, 15);
 		ind_tipo.setFont(fuente);
 		ind_tipo.setFont(ind_tipo.getFont().deriveFont(Font.BOLD, 15));
-		
-		String[] tipos = {"Acero", "Agua", "Bicho", "Dragon", "Electrico", "Fantasma",
-				"Fuego", "Hada", "Hielo", "Lucha", "Normal", "Planta", "Psiquico", "Roca",
-				"Siniestro", "Tierra", "Veneno", "Volador"};
-		
+
+		String[] tipos = { "Acero", "Agua", "Bicho", "Dragon", "Electrico", "Fantasma", "Fuego", "Hada", "Hielo",
+				"Lucha", "Normal", "Planta", "Psiquico", "Roca", "Siniestro", "Tierra", "Veneno", "Volador" };
+
 		campo_tipo = new JComboBox(tipos);
-		campo_tipo.setBounds(135, 192, 150, 20);
+		campo_tipo.setBounds(135, 230, 150, 20);
 		campo_tipo.setFont(fuente);
 		campo_tipo.setFont(campo_tipo.getFont().deriveFont(Font.BOLD, 12));
-		
+
 		ind_tipo_sec = new JLabel("2' TIPO");
-		ind_tipo_sec.setBounds(30, 213, 195, 15);
+		ind_tipo_sec.setBounds(30, 250, 195, 15);
 		ind_tipo_sec.setFont(fuente);
 		ind_tipo_sec.setFont(ind_tipo_sec.getFont().deriveFont(Font.BOLD, 15));
-		
-		String[] tipos_sec = {"Sin 2' Tipo", "Acero", "Agua", "Bicho", "Dragon", "Electrico", "Fantasma",
-				"Fuego", "Hada", "Hielo", "Lucha", "Normal", "Planta", "Psiquico", "Roca",
-				"Siniestro", "Tierra", "Veneno", "Volador"};
-		
+
+		String[] tipos_sec = { "Sin 2' Tipo", "Acero", "Agua", "Bicho", "Dragon", "Electrico", "Fantasma", "Fuego",
+				"Hada", "Hielo", "Lucha", "Normal", "Planta", "Psiquico", "Roca", "Siniestro", "Tierra", "Veneno",
+				"Volador" };
+
 		campo_tipo_sec = new JComboBox<String>(tipos_sec);
-		campo_tipo_sec.setBounds(135, 213, 150, 20);
+		campo_tipo_sec.setBounds(135, 251, 150, 20);
 		campo_tipo_sec.setFont(fuente);
 		campo_tipo_sec.setFont(campo_tipo_sec.getFont().deriveFont(Font.BOLD, 12));
-		
+
 		ind_descripcion = new JLabel("DESCRIPCION");
-		ind_descripcion.setBounds(30, 243, 195, 15);
+		ind_descripcion.setBounds(30, 290, 195, 15);
 		ind_descripcion.setFont(fuente);
 		ind_descripcion.setFont(ind_descripcion.getFont().deriveFont(Font.BOLD, 15));
-		
+
 		campo_descripcion = new JTextArea();
 		campo_descripcion.setEditable(true);
-		campo_descripcion.setBounds(30, 263, 270, 85);
+		campo_descripcion.setBounds(30, 320, 270, 85);
 		campo_descripcion.setBackground(Color.LIGHT_GRAY);
 		campo_descripcion.setLineWrap(true);
 		campo_descripcion.setWrapStyleWord(true);
 		campo_descripcion.setFont(fuente);
 		campo_descripcion.setFont(campo_descripcion.getFont().deriveFont(Font.BOLD, 12));
-		
+
 		confirmar = new JButton("CREAR");
-		confirmar.setBounds(95, 360, 150, 20);
+		confirmar.setBounds(95, 420, 150, 20);
 		confirmar.setBackground(Color.RED);
 		confirmar.setForeground(Color.WHITE);
 		confirmar.setFont(fuente);
 		confirmar.setFont(confirmar.getFont().deriveFont(Font.BOLD, 15));
-		
+
 		panel_creacion = new JPanel();
-		panel_creacion.setBounds(0,0, 330, 380);
+		panel_creacion.setBounds(0, 0, 330, 440);
 		panel_creacion.setBackground(Color.WHITE);
 		panel_creacion.setLayout(null);
 		add(ind_id);
@@ -195,6 +218,10 @@ public class PanelAgregar extends JPanel{
 		add(campo_nombre);
 		add(ind_lv);
 		add(campo_lv);
+		add(ind_al);
+		add(campo_al);
+		add(ind_pe);
+		add(campo_pe);
 		add(ind_hp);
 		add(campo_hp);
 		add(ind_atk);
@@ -214,15 +241,271 @@ public class PanelAgregar extends JPanel{
 		add(ind_descripcion);
 		add(campo_descripcion);
 		add(confirmar);
-		
+
 		panel_agregar = new JPanel();
-		panel_agregar.setBounds(5, 6, 360, 380);
+		panel_agregar.setBounds(5, 6, 360, 440);
 		panel_agregar.setBackground(Color.RED);
 		panel_agregar.setLayout(null);
 		panel_agregar.add(panel_creacion);
-		
+
 		add(panel_agregar);
 		setVisible(true);
 	}
-	
+
+	public JLabel getInd_al() {
+		return ind_al;
+	}
+
+	public void setInd_al(JLabel ind_al) {
+		this.ind_al = ind_al;
+	}
+
+	public JLabel getInd_pe() {
+		return ind_pe;
+	}
+
+	public void setInd_pe(JLabel ind_pe) {
+		this.ind_pe = ind_pe;
+	}
+
+	public JTextField getCampo_al() {
+		return campo_al;
+	}
+
+	public void setCampo_al(JTextField campo_al) {
+		this.campo_al = campo_al;
+	}
+
+	public JTextField getCampo_pe() {
+		return campo_pe;
+	}
+
+	public void setCampo_pe(JTextField campo_pe) {
+		this.campo_pe = campo_pe;
+	}
+
+	public JPanel getPanel_agregar() {
+		return panel_agregar;
+	}
+
+	public void setPanel_agregar(JPanel panel_agregar) {
+		this.panel_agregar = panel_agregar;
+	}
+
+	public JPanel getPanel_creacion() {
+		return panel_creacion;
+	}
+
+	public void setPanel_creacion(JPanel panel_creacion) {
+		this.panel_creacion = panel_creacion;
+	}
+
+	public JLabel getInd_id() {
+		return ind_id;
+	}
+
+	public void setInd_id(JLabel ind_id) {
+		this.ind_id = ind_id;
+	}
+
+	public JLabel getInd_nombre() {
+		return ind_nombre;
+	}
+
+	public void setInd_nombre(JLabel ind_nombre) {
+		this.ind_nombre = ind_nombre;
+	}
+
+	public JLabel getInd_lv() {
+		return ind_lv;
+	}
+
+	public void setInd_lv(JLabel ind_lv) {
+		this.ind_lv = ind_lv;
+	}
+
+	public JLabel getInd_hp() {
+		return ind_hp;
+	}
+
+	public void setInd_hp(JLabel ind_hp) {
+		this.ind_hp = ind_hp;
+	}
+
+	public JLabel getInd_atk() {
+		return ind_atk;
+	}
+
+	public void setInd_atk(JLabel ind_atk) {
+		this.ind_atk = ind_atk;
+	}
+
+	public JLabel getInd_def() {
+		return ind_def;
+	}
+
+	public void setInd_def(JLabel ind_def) {
+		this.ind_def = ind_def;
+	}
+
+	public JLabel getInd_satk() {
+		return ind_satk;
+	}
+
+	public void setInd_satk(JLabel ind_satk) {
+		this.ind_satk = ind_satk;
+	}
+
+	public JLabel getInd_sdef() {
+		return ind_sdef;
+	}
+
+	public void setInd_sdef(JLabel ind_sdef) {
+		this.ind_sdef = ind_sdef;
+	}
+
+	public JLabel getInd_vel() {
+		return ind_vel;
+	}
+
+	public void setInd_vel(JLabel ind_vel) {
+		this.ind_vel = ind_vel;
+	}
+
+	public JLabel getInd_tipo() {
+		return ind_tipo;
+	}
+
+	public void setInd_tipo(JLabel ind_tipo) {
+		this.ind_tipo = ind_tipo;
+	}
+
+	public JLabel getInd_tipo_sec() {
+		return ind_tipo_sec;
+	}
+
+	public void setInd_tipo_sec(JLabel ind_tipo_sec) {
+		this.ind_tipo_sec = ind_tipo_sec;
+	}
+
+	public JLabel getInd_descripcion() {
+		return ind_descripcion;
+	}
+
+	public void setInd_descripcion(JLabel ind_descripcion) {
+		this.ind_descripcion = ind_descripcion;
+	}
+
+	public JTextField getCampo_id() {
+		return campo_id;
+	}
+
+	public void setCampo_id(JTextField campo_id) {
+		this.campo_id = campo_id;
+	}
+
+	public JTextField getCampo_nombre() {
+		return campo_nombre;
+	}
+
+	public void setCampo_nombre(JTextField campo_nombre) {
+		this.campo_nombre = campo_nombre;
+	}
+
+	public JTextField getCampo_lv() {
+		return campo_lv;
+	}
+
+	public void setCampo_lv(JTextField campo_lv) {
+		this.campo_lv = campo_lv;
+	}
+
+	public JTextField getCampo_hp() {
+		return campo_hp;
+	}
+
+	public void setCampo_hp(JTextField campo_hp) {
+		this.campo_hp = campo_hp;
+	}
+
+	public JTextField getCampo_atk() {
+		return campo_atk;
+	}
+
+	public void setCampo_atk(JTextField campo_atk) {
+		this.campo_atk = campo_atk;
+	}
+
+	public JTextField getCampo_def() {
+		return campo_def;
+	}
+
+	public void setCampo_def(JTextField campo_def) {
+		this.campo_def = campo_def;
+	}
+
+	public JTextField getCampo_satk() {
+		return campo_satk;
+	}
+
+	public void setCampo_satk(JTextField campo_satk) {
+		this.campo_satk = campo_satk;
+	}
+
+	public JTextField getCampo_sdef() {
+		return campo_sdef;
+	}
+
+	public void setCampo_sdef(JTextField campo_sdef) {
+		this.campo_sdef = campo_sdef;
+	}
+
+	public JTextField getCampo_vel() {
+		return campo_vel;
+	}
+
+	public void setCampo_vel(JTextField campo_vel) {
+		this.campo_vel = campo_vel;
+	}
+
+	public JTextArea getCampo_descripcion() {
+		return campo_descripcion;
+	}
+
+	public void setCampo_descripcion(JTextArea campo_descripcion) {
+		this.campo_descripcion = campo_descripcion;
+	}
+
+	public JComboBox<String> getCampo_tipo() {
+		return campo_tipo;
+	}
+
+	public void setCampo_tipo(JComboBox<String> campo_tipo) {
+		this.campo_tipo = campo_tipo;
+	}
+
+	public JComboBox<String> getCampo_tipo_sec() {
+		return campo_tipo_sec;
+	}
+
+	public void setCampo_tipo_sec(JComboBox<String> campo_tipo_sec) {
+		this.campo_tipo_sec = campo_tipo_sec;
+	}
+
+	public JButton getConfirmar() {
+		return confirmar;
+	}
+
+	public void setConfirmar(JButton confirmar) {
+		this.confirmar = confirmar;
+	}
+
+	public Font getFuente() {
+		return fuente;
+	}
+
+	public void setFuente(Font fuente) {
+		this.fuente = fuente;
+	}
+
 }
