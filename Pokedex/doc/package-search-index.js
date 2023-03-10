19d68dd2d18bb6ba6f1d5e71cf851c7676f3c2f7
JoannZamudio11 <126726984+JoannZamudio11@users.noreply.github.com>
2023-03-09 19:25:30 -0500

    packageinfo
    javadoc de paquetes

--- Pokedex/doc/package-search-index.js
+++ Pokedex/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"co.edu.unbosque.model"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"co.edu.unbosque.controller"},{"l":"co.edu.unbosque.model"},{"l":"co.edu.unbosque.view"}];updateSearchResults();
